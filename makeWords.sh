9390f5c505e9376058df253be998f3eaf3faaa1d
Jade Lovelace <software@lfcode.ca>
2022-01-29 11:32:29 -0800

    add some gender words

--- makeWords.sh
+++ makeWords.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 cat /usr/share/dict/british-english words-raw corrects | grep -E '^[A-Za-z]+$' | tr A-Z a-z | sort -u > words
+# sick of getting spoiled
+cat corrects-soon corrects-later > corrects
