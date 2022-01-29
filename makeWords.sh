3fede87636cef5d5bbc3d9db6af54af37dfcaa86
Jade Lovelace <software@lfcode.ca>
2022-01-29 15:31:08 -0800

    whoops, get data dependency right

--- makeWords.sh
+++ makeWords.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cat /usr/share/dict/british-english words-raw corrects | grep -E '^[A-Za-z]+$' | tr A-Z a-z | sort -u > words
 # sick of getting spoiled
 cat corrects-soon corrects-later > corrects
+
+cat /usr/share/dict/british-english words-raw corrects | grep -E '^[A-Za-z]+$' | tr A-Z a-z | sort -u > words
