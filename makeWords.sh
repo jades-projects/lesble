1a5f291465426836a7d9f13628dac897b9ab61ad
Jade Lovelace <software@lfcode.ca>
2022-02-09 14:50:05 -0800

    add words

--- makeWords.sh
+++ makeWords.sh
@@ -2,4 +2,4 @@
 # sick of getting spoiled
 cat corrects-soon corrects-later > corrects
 
-cat /usr/share/dict/british-english words-raw corrects | grep -E '^[A-Za-z]+$' | tr A-Z a-z | sort -u > words
+cat /usr/share/dict/british-english words-raw corrects adds | grep -E '^[A-Za-z]+$' | tr A-Z a-z | sort -u > words
