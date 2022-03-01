a89e371831a00a5f1043c4c09f6897cf101b6ce0
lf- <lf-@users.noreply.github.com>
2022-03-01 08:24:08 +0000

    deploy: 9bd036e1072faa2843e69815d5dd46ebb2205178

--- out/main.js
+++ out/main.js
@@ -1,6 +1,6 @@
 import * as T from "./template.js";
 const GUESSES = 6;
-const COMMIT = 'fd9494b';
+const COMMIT = '9bd036e';
 var LetterColour;
 (function (LetterColour) {
     LetterColour[LetterColour["GREEN"] = 0] = "GREEN";
--- ts/main.ts
+++ ts/main.ts
@@ -1,7 +1,7 @@
 import * as T from "./template.js";
 
 const GUESSES = 6;
-const COMMIT = 'fd9494b';
+const COMMIT = '9bd036e';
 
 enum LetterColour {
     GREEN,
