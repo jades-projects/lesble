dc9dd1c99ef7857f724fa9d8dfe4ad6a9ddc4b94
Jade Lovelace <software@lfcode.ca>
2024-11-14 18:18:55 -0800

    restart, again

--- enddate.py
+++ enddate.py
@@ -3,6 +3,6 @@ import datetime
 with open('corrects') as fh:
     words = [l for l in fh.readlines() if l.strip() != '']
 
-end_date = datetime.date(2023, 1, 6) + datetime.timedelta(days=len(words))
+end_date = datetime.date(2024, 11, 14) + datetime.timedelta(days=len(words))
 print(end_date.isoformat())
 print('days: ', end_date - datetime.date.today())
